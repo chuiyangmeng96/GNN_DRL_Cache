ad1160f9ce82ef0b4c7f4c522656f30496d3326d
chuiyangmeng96 <chuiyangmeng@outlook.com>
2021-04-12 16:36:15 +0800

    action space modified

--- environment.py
+++ environment.py
@@ -823,7 +823,8 @@ class Environ:
         return total_delay
 
     def action_space(self): # action_matrix is a matrix of size self.vehicle_num * (self.vehicle_num + self.RSU_num + 1)
-        action_matrix = np.zeros(self.vehicle_num)
+        action_matrix = np.random.rand(np.zeros(self.vehicle_num))
+
         return action_matrix
 
     def immediate_reward(self, action_matrix):
